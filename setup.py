5adcb008963f6d9eedf1498a04a3a1d0f81d87d4
David Albert <davidbalbert@gmail.com>
2012-04-09 18:27:54 -0400

    fix setup.py on Mac

--- setup.py
+++ setup.py
@@ -80,7 +80,7 @@ def make_universal_header(filename, *defines):
                 if line.startswith(prefix):
                     line = '#ifdef __LP64__\n#define %s 8\n#else\n#define %s 4\n#endif' % (define, define)
                     break
-        print >>f, line
+        f.write(line + "\n")
     f.close()
 
 
